f0a06c3d77c9be28b462a251dd5af5ed18548a99
Nihilistzsche <nihilistzsche@gmail.com>
2023-12-25 04:43:41 -0800

    Lua is a magical language.

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,4 +1,17 @@
 if mods["bzaluminum"] then
-    data.raw.technology["dmr18-repl-aluminum-2219"].name = "dmr18-repl-aluminum-2219-dmr"
-    data.raw.technology["dmr18-repl-aluminum-6061"].name = "dmr18-repl-aluminum-6061-dmr"
+    data.raw.technology["dmr18-repl-aluminum-2219-dmr"] = table.deepcopy(data.raw.technology["dmr18-repl-aluminum-2219"])
+    data.raw.technology["dmr18-repl-aluminum-2219-dmr"].name = "dmr18-repl-aluminum-2219-dmr"
+    data.raw.technology["dmr18-repl-aluminum-2219"] = nil
+    data.raw.technology["dmr18-repl-aluminum-6061-dmr"] = table.deepcopy(data.raw.technology["dmr18-repl-aluminum-6061"])
+    data.raw.technology["dmr18-repl-aluminum-6061-dmr"].name = "dmr18-repl-aluminum-6061-dmr"
+    data.raw.technology["dmr18-repl-aluminum-6061"] = nil
+    for tech, ttbl in pairs(data.raw.technology) do
+        if ttbl.prerequisites then
+            for i, prereq in pairs(ttbl.prerequisites) do
+                if prereq == "dmr18-repl-aluminum-2219" or prereq == "dmr18-repl-aluminum-6061" then
+                    ttbl.prerequisites[i] = prereq .. "-dmr"
+                end
+            end
+        end
+    end
 end
